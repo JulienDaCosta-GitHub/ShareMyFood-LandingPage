dccf27e3f7d0742aa10ddc5929b105976459b3d7
Julien Da Costa <juliendacosta27@gmail.com>
2023-05-05 14:44:30 +0200

    goals and motivations

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import HeaderComponent from './components/HeaderComponent.vue'
 import MainSection from "./components/MainSection.vue";
 import WhatIsShareMyFood from "@/components/WhatIsShareMyFood.vue";
+import GoalsAndMotivations from "@/components/GoalsAndMotivations.vue";
 </script>
 
 <template>
   <HeaderComponent></HeaderComponent>
   <MainSection></MainSection>
   <WhatIsShareMyFood></WhatIsShareMyFood>
+  <GoalsAndMotivations></GoalsAndMotivations>
 </template>
 
 <style scoped>
---
+++ src/components/GoalsAndMotivations.vue
@@ -0,0 +1,77 @@
+<template>
+<div class="goals-and-motivations">
+  <div class="goals-and-motivations-container">
+    <h2>Nos objectifs et motivations</h2>
+
+    <div class="goals-and-motivations-lists">
+      <div class="goals-and-motivations-each-list">
+        <h3 class="goals-and-motivations-lists-title">Nos objectifs</h3>
+        <div>
+          <ul>
+            <li class="goals-and-motivations-lists-title-li">Réduire le gaspillage alimentaire en permettant aux utilisateurs de donner ou vendre leurs aliments non consommés à d'autres personnes qui en ont besoin.</li>
+            <li class="goals-and-motivations-lists-title-li">Encourager le partage et la solidarité entre les membres de la communauté grâce à une application conviviale et facile à utiliser.</li>
+            <li class="goals-and-motivations-lists-title-li">Contribuer à la protection de l'environnement en réduisant la production de déchets alimentaires.</li>
+          </ul>
+        </div>
+      </div>
+      <div class="goals-and-motivations-each-list">
+        <h3 class="goals-and-motivations-lists-title">Nos motivations</h3>
+        <div>
+          <ul>
+            <li class="goals-and-motivations-lists-title-li">Sensibiliser les gens à l'importance de la réduction du gaspillage alimentaire et de la protection de l'environnement.</li>
+            <li class="goals-and-motivations-lists-title-li">Offrir une solution pratique et efficace pour les personnes qui cherchent à donner ou à récupérer de la nourriture non consommée.</li>
+            <li class="goals-and-motivations-lists-title-li">Favoriser le développement d'une communauté solidaire et engagée dans la lutte contre le gaspillage alimentaire.</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+
+    <div class="goals-and-motivations-img">
+      <img src="src/assets/img/planet.png" alt="planet">
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: "GoalsAndMotivations"
+}
+</script>
+
+<style scoped>
+.goals-and-motivations {
+  min-height: 700px;
+  padding: 80px 250px;
+}
+
+.goals-and-motivations h2 {
+  margin-bottom: 50px;
+  font-size: 22px;
+  font-weight: bold;
+}
+
+.goals-and-motivations-lists {
+  display: flex;
+  justify-content: space-between;
+}
+
+.goals-and-motivations-lists-title {
+  font-weight: bold;
+  font-size: 18px;
+  margin-bottom: 20px;
+}
+
+.goals-and-motivations-lists-title-li {
+  margin-bottom: 15px;
+}
+
+.goals-and-motivations-each-list {
+  width: 500px;
+}
+
+.goals-and-motivations-img {
+  text-align: center;
+  margin-top: 50px;
+}
+</style>
